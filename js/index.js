bfe74b069104f5f9249aaaf2401c6737555c47f4
annomao <omaoannkerubo@gmail.com>
2022-04-20 10:46:26 +0300

    replace review form with form get city form

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-  
-})
+// document.addEventListener("DOMContentLoaded", ()=>{
+
+//   const appKey = 'd80e6647e32d60a5868d475d584ac6f2';
+//   fetch(`https://api.openweathermap.org/data/2.5/weather?q=Nairobi&appid=${appKey}`)
+//   .then(res => res.json())
+//   .then (data => {
+//     console.log(data)
+//   })
+
+// })
