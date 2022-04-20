450faf3a94979223480e84401c94b23fc480b511
annomao <omaoannkerubo@gmail.com>
2022-04-20 12:00:22 +0300

    add function to get weather on city input

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
-// document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", ()=>{
+  getWeatherData()
+})
 
-//   const appKey = 'd80e6647e32d60a5868d475d584ac6f2';
-//   fetch(`https://api.openweathermap.org/data/2.5/weather?q=Nairobi&appid=${appKey}`)
-//   .then(res => res.json())
-//   .then (data => {
-//     console.log(data)
-//   })
+function getWeatherData(){
+  const displaySection = document.querySelector("#display-section")
+  const cityForm = document.querySelector("#city-form")
+  const city = document.querySelector("#city")
+  cityForm.addEventListener("submit",(event)=>{
 
-// })
+    event.preventDefault()
+
+    const cityName = city.value
+    const appKey = 'd80e6647e32d60a5868d475d584ac6f2';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appKey}`)
+    .then(res => res.json())
+    .then (data => {
+      console.log(data)
+    })
+    event.target.reset()
+  })
+}
